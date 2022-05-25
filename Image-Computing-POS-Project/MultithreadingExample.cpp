fc594e64965fac662afbb95196cebf0d64ded20b
matizoxx <matizoxx21@gmail.com>
2022-05-25 14:39:01 +0200

    added 1. dynamic thread creator 2. mutex locking

--- Image-Computing-POS-Project/MultithreadingExample.cpp
+++ Image-Computing-POS-Project/MultithreadingExample.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 
 using namespace std;
 
@@ -7,31 +8,45 @@ bool finish = false;
 
 const auto processors = std::thread::hardware_concurrency();
 
-void Work()
+mutex mymutex;
+thread* worker = new thread[processors];
+
+void Work(int n)
 {
 	using namespace literals::chrono_literals;
-
+	
 	while (!finish)
 	{
-		cout << "Praca, praca...\n";
-		this_thread::sleep_for(1s);
+		mymutex.lock(); //do lockowania zapisu na dysk
+		cout << "Thread" << n + 1 << "working. ID=" << worker[n].get_id() << endl;
+		//this_thread::sleep_for(0.5s);
+		mymutex.unlock();
 	}
+	
 }
 
 int main()
 {
 	cout << processors << endl;
 
-	thread worker(Work);
-	//thread worker1(Work);
+	for (int i = 0; i < processors; i++)
+	{
+		worker[i] = thread(Work, i);
+	}
 
 	cin.get();
 	finish = true;
 
-	worker.join();
-	//worker1.join();
-	cout << "Koniec.\n";
+	for (int i = 0; i < processors; i++)
+	{
+		worker[i].join();
+	}
+
+	cout << "Finish.\n";
 
 	cin.get();
+	
+	delete[] worker;
+
 	return 0;
 }
