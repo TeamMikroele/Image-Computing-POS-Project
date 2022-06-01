95e2acdc8a1993befdc9a12ae8681503145aeb9f
matizoxx <matizoxx21@gmail.com>
2022-06-02 01:59:57 +0200

    added multithreading with mutex IPC

--- Image-Computing-POS-Project/Main.cpp
+++ Image-Computing-POS-Project/Main.cpp
@@ -3,65 +3,15 @@
 #include <mutex>
 #include "ini_parser.h"
 #include "img_proces.h"
+#include "multithread.h"
 
 using namespace std;
 
-auto processors = std::thread::hardware_concurrency();
-
-mutex mymutex;
-thread* worker = new thread[processors];
-
-void Work(int n)
-{
-	using namespace literals::chrono_literals;
-	//mymutex.lock(); //do lockowania zapisu na dysk
-	edge_detecting(n);
-	//mymutex.unlock();
-
-}
-
 int main() {
-
-	cout << processors << endl;
-
-	
 	
 	ini_read();
-	read_input_folder_info();
-	for (int i = 0; i < size(Image); i++)img_read(i);
-	int obrazki = size(Image) - 1;
+	multithread();
 
-	int iteracje = size(Image) / processors;
-	if (size(Image) % processors)
-	{
-		iteracje++;
-	}
-
-	
-	for (int i = 0; i < iteracje; i++)
-	{
-		if (i == iteracje - 1 && size(Image) % processors != 0)
-			processors = size(Image) % processors;
-		for (int j = 0; j < processors; j++)
-		{
-			worker[j] = thread(Work, obrazki);
-			obrazki--;
-		}
-
-		for (int j = 0; j < processors; j++)
-		{
-			worker[j].join();
-		}
-	}
-
-	
-
-	//for (int i =0; i<size(Image); i++) Image[i] = edge_detecting(Image[i]);
-
-	for (int i = 0; i < size(Image); i++)img_write(i);
-	
-	delete[] worker;
 	system("PAUSE");
 	return 0;
-
 }
---
+++ Image-Computing-POS-Project/multithread.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include "img_proces.h"
+
+using namespace std;
+
+auto processors = std::thread::hardware_concurrency();
+
+mutex mymutex;
+thread* worker = new thread[processors];
+
+void Work(int n)
+{
+	img_read(n);
+	edge_detecting(n);
+	mymutex.lock();
+	img_write(n);
+	mymutex.unlock();
+}
+
+void multithread() {
+
+	cout << "Number of threads: " << processors << endl;
+
+	read_input_folder_info();
+
+	int images = size(Image) - 1;
+
+	int iterations = size(Image) / processors;
+	if (size(Image) % processors)
+	{
+		iterations++;
+	}
+
+	for (int i = 0; i < iterations; i++)
+	{
+		if (i == iterations - 1 && size(Image) % processors != 0)
+			processors = size(Image) % processors;
+		for (int j = 0; j < processors; j++)
+		{
+			worker[j] = thread(Work, images);
+			images--;
+		}
+
+		for (int j = 0; j < processors; j++)
+		{
+			worker[j].join();
+		}
+	}
+	delete[] worker;
+}
